d8980b7c5aed0050d5a4f1877b81309c55bbc4ed
Shree <Shree@DESKTOP-VAQFKMT>
2024-11-23 10:59:00 -0800

    Online Payment feature added

---
+++ Proj01/src/main/java/com/nt/sbeans/PhonePe.java
@@ -0,0 +1,11 @@
+package com.nt.sbeans;
+
+public class PhonePe implements OnlinePayment {
+
+	@Override
+	public String paymentMethod() {
+		// TODO Auto-generated method stub
+		return "Phonpe Payment";
+	}
+
+}
