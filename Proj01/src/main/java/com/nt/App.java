6529a2cb45fd2e9c72d21c630184d772c0a85cec
Shree <Shree@DESKTOP-VAQFKMT>
2024-11-23 09:43:58 -0800

    Project initialization

---
+++ Proj01/src/main/java/com/nt/App.java
@@ -0,0 +1,15 @@
+package com.nt;
+
+/**
+ * Hello world!
+ */
+public class App {
+	public String orderItems(String item[],double bill[])
+	{
+		return item.toString()+" bill for orderd items :"+bill.toString();
+	}
+   
+	public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
