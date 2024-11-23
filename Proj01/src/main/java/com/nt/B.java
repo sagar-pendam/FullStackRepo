fd45d4acf8476e459197e8663a3b94187ed84e1b
Shree <Shree@DESKTOP-VAQFKMT>
2024-11-23 10:07:43 -0800

    B logic added

---
+++ Proj01/src/main/java/com/nt/B.java
@@ -0,0 +1,17 @@
+package com.nt;
+
+import java.util.List;
+
+public class B {
+
+	public B()
+	{
+		System.out.println("NPC");
+	}
+	
+	public String itemList()
+	{
+		String[] items = {"I Phone 18 Pro","Philips hair dryer"};
+		return items.toString();
+	}
+}
